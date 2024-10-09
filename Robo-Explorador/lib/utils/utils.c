9bfd3b7f9346223a659751fe57f13608c416a85f
ASBeatriz <beatriz.alvessantos05@gmail.com>
2024-10-09 20:05:26 -0300

    Add utils

---
+++ Robo-Explorador/lib/utils/utils.c
@@ -0,0 +1,7 @@
+#include "utils.h"
+
+int TICKS_MOTOR_RIGHT = 0;
+int TICKS_MOTOR_LEFT = 0;
+
+float TARGET_VEL_RIGHT = 0;
+float TARGET_VEL_LEFT = 0;
---
+++ Robo-Explorador/lib/utils/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// leitura do motor - mandar para comunicação
+extern int ENCODER_READ_L = 0;
+extern int ENCODER_READ_R = 0;
+
+// velocidade alvo - receber da comunicação
+extern float TARGET_VEL_R;
+extern float TARGET_VEL_L;
+
+#endif
